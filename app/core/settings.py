f640948bb6232afe9cc237d2071d772b2bbba429
suphanatchanlek <suphanatchanlek@gmail.com>
2026-04-23 18:06:52 +0700

    update

--- app/core/settings.py
+++ app/core/settings.py
@@ -17,7 +17,10 @@ class Settings(BaseSettings):
             "http://localhost:3000,"
             "http://127.0.0.1:3000,"
             "http://localhost:5173,"
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173,"
+            "http://localhost:5500,"
+            "http://127.0.0.1:5500,"
+            "https://event-around.vercel.app"
         )
     )
 
